f401dfac1e68f7b299ea8f9a4c115dc398990bb0
jariseon <jari.kleimola@outlook.com>
2017-12-10 20:55:09 +0200

    hardcoded buffersize to 128 and fixed slicing

--- audioworker.js
+++ audioworker.js
@@ -53,22 +53,28 @@ AWGS.AudioWorkletGlobalScope = function () {
 AudioWorkletGlobalScope = AWGS.AudioWorkletGlobalScope();
 AudioWorkletProcessor   = AudioWorkletGlobalScope.AudioWorkletProcessor;
 registerProcessor = AudioWorkletGlobalScope.registerProcessor;
+sampleRate = 44100;
 
 onmessage = function (e) {
   var msg = e.data;
   switch (msg.type) {
 
+    case "init":
+      sampleRate = AudioWorkletGlobalScope.sampleRate = msg.sampleRate;
+      break;
+      
     case "import":
       importScripts(msg.url);
       postMessage({ type:"load", url:msg.url });
       break;
       
     case "createProcessor":
-      // -- if required, slice io for double buffering
+      // -- slice io to match with SPN bufferlength
       var a = msg.args;
-      var pingpong = [];
-      var buflen = a.options.samplesPerBuffer;
-      for (var i=0; i<a.bus.bufferCount; i++) {
+      var slices = [];
+      var buflen = 128;
+      var numSlices = (a.options.samplesPerBuffer/buflen)|0;
+      for (var i=0; i<numSlices; i++) {
         var sliceStart = i * buflen;
         var sliceEnd   = sliceStart + buflen;
         
@@ -88,23 +94,25 @@ onmessage = function (e) {
         var inbus  = createBus(a.bus.input);
         var outbus = createBus(a.bus.output);
         
-        pingpong.push({ inbus:inbus, outbus:outbus });
+        slices.push({ inbus:inbus, outbus:outbus });
       }
 
       // -- create processor
-      AudioWorkletGlobalScope.sampleRate = a.sampleRate;
       var processor = AudioWorkletGlobalScope._createProcessor(a.name, e.ports[0], a.options);
       processor.node = a.node;
       processor.id = AWGS.processors.length;
-      AWGS.processors.push({ awp:processor, slices:pingpong });
+      processor.numSlices = numSlices;
+      AWGS.processors.push({ awp:processor, slices:slices });
       postMessage({ type:"state", node:a.node, processor:processor.id, state:"running" });
       break;
       
     case "process":
       var processor = AWGS.processors[msg.processor];
       if (processor) {
-        var slice = processor.slices[msg.index];
-        processor.awp.process(slice.inbus, slice.outbus);
+        for (var i=0; i<processor.slices.length; i++) {
+          var slice = processor.slices[i];
+          processor.awp.process(slice.inbus, slice.outbus, []);
+        }
       }
       break;
   }
