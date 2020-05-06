099cca567dcef076e7552d15b88dbd61483b9a8d
Muhammad Salar Khan <muhammedsalarkhan@gmail.com>
2020-05-06 08:40:05 +0500

    Format

--- audioprocessor.js
+++ audioprocessor.js
@@ -1,13 +1,13 @@
 class Processor extends AudioWorkletProcessor {
     constructor(options) {
-	super(options);
+        super(options);
         this.port.onmessage = (e) => {
-            if(e.data === "stop") {
+            if (e.data === "stop") {
                 this.resume = false;
             }
         }
-	this.ticks = 0;
-	this.resume = true;
+        this.ticks = 0;
+        this.resume = true;
     }
 
     process(inputs, outputs, parameters) {
@@ -16,20 +16,20 @@ class Processor extends AudioWorkletProcessor {
         const channels = outputs[0];
         const outputChannel1 = channels[0];
         const outputChannel2 = channels[1];
-	
-	// Send a message after every 1000 ticks
-	this.ticks += 1;
-	if(this.ticks === 1000) {
-	    this.port.postMessage("audioprocessor is alive."); 
-	    this.ticks = 0;
-	}
 
-	outputChannel1.set(inputChannel1, 0);
-	outputChannel2.set(inputChannel1, 0);
-	if(!this.resume) {
-	    this.port.postMessage("Bye Bye! from audioprocessor.");
-	}
-	
+        // Send a message after every 1000 ticks
+        this.ticks += 1;
+        if (this.ticks === 1000) {
+            this.port.postMessage("audioprocessor is alive.");
+            this.ticks = 0;
+        }
+
+        outputChannel1.set(inputChannel1, 0);
+        outputChannel2.set(inputChannel1, 0);
+        if (!this.resume) {
+            this.port.postMessage("Bye Bye! from audioprocessor.");
+        }
+
         return this.resume;
     }
 }
